a7074120dd6df83dc99bd18774eb2517ec9dac44
Shruti Vangari <shruti.vangari@cerner.com>
2015-10-29 13:42:19 -0500

    Adding show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/tags' => 'tags#index'
+  get '/tags/:id' => 'tags#show', as: :tag
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
