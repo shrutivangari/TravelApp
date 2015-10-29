9e4a3d9d0f6587990992230d27e8c045849db6e0
Shruti Vangari <shruti.vangari@cerner.com>
2015-10-29 13:33:27 -0500

    Adding columns, foreign key, migration

--- db/migrate/20151029183037_create_tags.rb
+++ db/migrate/20151029183037_create_tags.rb
@@ -1,7 +1,8 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
-
+      t.string :title
+      t.string :image
       t.timestamps null: false
     end
   end
--- db/migrate/20151029183056_create_destinations.rb
+++ db/migrate/20151029183056_create_destinations.rb
@@ -1,7 +1,10 @@
 class CreateDestinations < ActiveRecord::Migration
   def change
     create_table :destinations do |t|
-
+      t.string :name
+      t.string :image
+      t.string :description
+      t.references :tag
       t.timestamps null: false
     end
   end
